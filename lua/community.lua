bb5b1472f3875994fdaf10276b0d84e5b89edcd5
lxrdxe7o <ishrak7106@gmail.com>
2025-02-07 01:29:32 +0600

    made repo

---
+++ lua/community.lua
@@ -0,0 +1,38 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+
+  { import = "astrocommunity.pack.lua" },
+
+  { import = "astrocommunity.pack.java" },
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-23",
+                path = "/usr/lib/jvm/java-23-openjdk/",
+              },
+            },
+          },
+        },
+        format = {
+          enabled = true,
+          settings = { -- you can use your preferred format style
+            url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+            profile = "GoogleStyle",
+          },
+        },
+      },
+    },
+  },
+
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+}
