1fb40fcf555656807ced3a59c54c488fb5ae686c
xero <ishrak7106@gmail.com>
2025-08-19 12:35:27 +0600

    added nvim-java

--- lua/community.lua
+++ lua/community.lua
@@ -8,9 +8,9 @@ return {
 
   { import = "astrocommunity.pack.lua" },
 
-  { import = "astrocommunity.pack.java" },
-  -- { import = "astrocommunity.lsp.nvim-java" },
-
+  -- { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.lsp.nvim-java" },
+  --[[
   {
     "mfussenegger/nvim-jdtls",
     opts = {
@@ -39,6 +39,7 @@ return {
       },
     },
   },
+  ]]
   {
     "xero/miasma.nvim",
     lazy = false,
