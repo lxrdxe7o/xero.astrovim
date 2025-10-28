67345dbb6f009ff1cbcc8ccd2565db69c3a08bce
xero <ishrak7106@gmail.com>
2025-10-29 01:49:19 +0600

    changed init.lua

--- lua/community.lua
+++ lua/community.lua
@@ -50,6 +50,65 @@ return {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
+    --[[
+require("cyberdream").setup {
+  -- Set light or dark variant
+  variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+
+  -- Enable transparent background
+  transparent = true,
+
+  -- Reduce the overall saturation of colours for a more muted look
+  saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
+
+  -- Enable italics comments
+  italic_comments = true,
+
+  -- Replace all fillchars with ' ' for the ultimate clean look
+  hide_fillchars = false,
+
+  -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
+  borderless_pickers = true,
+
+  -- Set terminal colors used in `:terminal`
+  terminal_colors = true,
+
+  -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+  cache = true,
+
+  extensions = {
+    alpha = true,
+    blinkcmp = true,
+    cmp = true,
+    dashboard = true,
+    fzflua = true,
+    gitpad = true,
+    gitsigns = true,
+    grapple = true,
+    grugfar = true,
+    heirline = true,
+    helpview = true,
+    hop = true,
+    indentblankline = true,
+    kubectl = true,
+    lazy = true,
+    leap = true,
+    markdown = true,
+    markview = true,
+    mini = true,
+    noice = true,
+    neogit = true,
+    notify = true,
+    rainbow_delimiters = true,
+    snacks = true,
+    telescope = true,
+    treesitter = true,
+    treesittercontext = true,
+    trouble = true,
+    whichkey = true,
+  },
+}
+]]
   },
   { import = "astrocommunity.colorscheme.gruvbox-nvim" },
   {
